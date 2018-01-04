b9049fccc90c9107afe9e9bac4409c7266a94a41
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2018-01-04 12:08:53 +0000

    repo init

---
+++ js/board-math.js
@@ -0,0 +1,96 @@
+/*
+ * Funcionalidad de tu producto
+ */
+//
+// function getEnrollment(office, generation) {
+//   return data[office][generation]["students"].length;
+// }
+//
+// function getActiveStudentsPerGeneration(office, generation) {
+//   var activeCount = 0;
+//   var students = data[office][generation]["students"];
+//   for (var i = 0; i < students.length; i++) {
+//     var student = students[i];
+//     if (student.active === true) {
+//       activeCount++;
+//     }
+//   }
+//   return activeCount;
+// }
+//
+// function getPercentage(numerator, denominator) {
+//   return Math.floor(numerator / denominator * 100);
+// }
+//
+// function getDropout(office, generation) {
+//   var totalStudents = getEnrollment(office, generation);
+//   var activeStudents = getActiveStudentsPerGeneration(office, generation);
+//   var unactiveStudents = totalStudents - activeStudents;
+//   return getPercentage(unactiveStudents, totalStudents);
+// }
+//
+// function getNPSOfSprint(office, generation, sprint) {
+//   var npsData = data[office][generation]["ratings"][sprint - 1];
+//   return npsData.promoters - npsData.detractors;
+// }
+//
+// function getNPS(office, generation) {
+//   var sprintNumbersOfGeneration = data[office][generation]["ratings"].length;
+//   var npsCount = 0;
+//
+//   for (var i = 1; i < sprintNumbersOfGeneration; i++) {
+//     var npsOfSprint = getNPSOfSprint(office, generation, i);
+//     npsCount += npsOfSprint;
+//     console.log(npsOfSprint);
+//   }
+//
+//   return npsCount / sprintNumbersOfGeneration;
+// }
+
+var select = document.getElementById("select");
+select.addEventListener("change", filter);
+
+function filter() {
+  //como hacemos para que filter haga una sola cosa?
+  var selectedIndex = event.target.selectedIndex;
+  var selectedOption = event.target[selectedIndex];
+  var office = selectedOption.dataset.sede;
+  var generation = selectedOption.dataset.generation;
+
+  var studentsCount = getStudentsCount(office, generation);
+  var activeStudents = getActiveStudents(office, generation);
+  var inactiveStudents = getInactiveStudents(office, generation);
+  var dropoutPercents = Math.round(inactiveStudents / studentsCount * 100);
+  var percentageDesertion = convertDesertionNumberToPercentage(studentsCount, inactiveStudents);
+
+  document.getElementById("students-enrolled").innerText = studentsCount;
+  document.getElementById("students-dropout").innerText = dropoutPercents;
+
+  paintData(activeStudents, percentageDesertion);
+}
+
+function getStudentsCount(office, generation) {
+  return data[office][generation]["students"].length;
+}
+
+function getActiveStudents(office, generation) {
+  var students = data[office][generation]["students"];
+  var studentActiveCount = 0;
+  for (var i = 0; i < students.length; i++) {
+    if (students[i]["active"] === true) {
+      studentActiveCount++;
+    }
+  }
+  return studentActiveCount;
+}
+
+function getInactiveStudents(office, generation) {
+  var inactiveStudents = getStudentsCount(office, generation) - getActiveStudents(office, generation); 
+  return inactiveStudents;
+} 
+
+function convertDesertionNumberToPercentage(studentsCount, inactiveStudents) {
+  var percentageDesertion = (inactiveStudents * 100) / studentsCount;
+  return percentageDesertion;
+}
+
---
+++ js/draw-results.js
@@ -0,0 +1,23 @@
+var select = document.getElementById("select");
+select.addEventListener("change", filter);
+
+// function updateBoard() {
+//   var selectedIndex = event.target.selectedIndex;
+//   var currentOption = event.target[selectedIndex];
+//   var office = currentOption.dataset.sede;
+//   var generation = currentOption.dataset.generation;
+//   var enrollment = getEnrollment(office, generation);
+//   var dropout = getDropout(office, generation);
+//   var nps = getNPS(office, generation);
+//   console.log("enrollment", enrollment);
+//   console.log("dropout", dropout);
+//   console.log("nps", nps);
+// }
+
+function paintData(activeStudents, percentageDesertion) {
+    var containerEnrolledStudents = document.getElementById('students-enrolled');
+    var containerDesertion = document.getElementById('students-dropout');
+    containerEnrolledStudents.innerText = activeStudents;
+    containerDesertion.innerHTML = percentageDesertion;
+    console.log('Hola');
+}
